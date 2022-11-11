686522e1d3431e9b78dba06129d70973ad387b5e
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-11-11 19:03:01 +0100

    settings localStorage changes

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ const cupVolumes = document.querySelectorAll('.CupVol')
 const progressBar = document.querySelector('.bar')
 const currentVolume = document.querySelector('#currentVolume')
 let progressValue = document.querySelector('progress')
-console.log(progressValue.value)
+const subButton = document.querySelector('.submit')
 cupVolumes.forEach((el) => {
     el.addEventListener('click', FillProgressBar)
 })
+subButton.addEventListener('click', grabValuesFromSettingsFormAndAddToLocalStorage)
 
 function FillProgressBar() {
     let progress = Number(this.innerText.slice(0,-3))
@@ -14,7 +15,19 @@ function FillProgressBar() {
     currentVolume.innerText = `${progressValue.value} ml`
 }
 
+function grabValuesFromSettingsFormAndAddToLocalStorage(){
+    let name = document.querySelector('#name').value
+    let weight = document.querySelector('#weight').value
+    let gender = document.querySelector('#gender').value
+    let cupSize = document.querySelector('#cupSize').value
+    console.log(name, weight, gender, cupSize)
+    localStorage.setItem('name', `${name}`);
+    localStorage.setItem('weight', `${weight}`);
+    localStorage.setItem('gender', `${gender}`);
+    localStorage.setItem('cupSize', `${cupSize}`);
+}
 
+// const userName = document.querySelector('#userName')
+// userName.innerHTML = localStorage.getItem("name");
 
-
-//console.log(el.innerText.slice(0,-3))
+//https://stackoverflow.com/questions/40598081/how-to-populate-an-html-input-field-from-html-localstorage
