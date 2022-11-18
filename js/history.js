7c34f064b2957800484a53f1e7eaaee68cdeb90a
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-11-18 19:21:42 +0100

    working slowly on history tab

--- js/history.js
+++ js/history.js
@@ -1,5 +1,14 @@
+const calendar = {
+    monday : document.querySelector('.monday'),
+    mondaySpan : document.querySelector('.monday--span')
+}
 
+const data = new Date()
 
+console.log(data.getDay())
+
+calendar.monday.style.height = `${localStorage.getItem('progress')/10}px`
+calendar.mondaySpan.innerText = `${localStorage.getItem('progress')}ml`
 
 
 
