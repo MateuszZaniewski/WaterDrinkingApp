57c12a14838c44f7aa8763d89c01d9bfae093034
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-11-19 14:25:26 +0100

    working checking for current day

--- js/history.js
+++ js/history.js
@@ -1,36 +1,54 @@
 const calendar = {
-    monday : document.querySelector('.monday'),
-    mondaySpan : document.querySelector('.monday--span'),
     dataSpan : document.querySelectorAll('.calendar__ml--span'),
+    values : {
+        value1 : document.querySelector('.dataValue1'),
+        value2 : document.querySelector('.dataValue2'),
+        value3 : document.querySelector('.dataValue3'),
+        value4 : document.querySelector('.dataValue4'),
+        value5 : document.querySelector('.dataValue5'),
+        value6 : document.querySelector('.dataValue6'),
+    },
+    days : [document.querySelector('.monday'), 
+            document.querySelector('.thuesday'),
+            document.querySelector('.wednesday'),
+            document.querySelector('.thursday'),
+            document.querySelector('.friday'),
+            document.querySelector('.saturnday'),
+            document.querySelector('.sunday'),
+    ],
 }
 
-const data = new Date()
 
-calendar.monday.style.height = `${localStorage.getItem('progress')/10}px`
-calendar.mondaySpan.innerText = `${localStorage.getItem('progress')}ml`
+function findAndSetValuesForToday () {
+    const data = new Date()
+    for(let i = 0; i < 7; i++){
+        if(data.getDay() == i){
+            localStorage.setItem(`day${i}`, `${localStorage.getItem('progress')}`)
+            calendar.days[i-1].style.height = `${localStorage.getItem(`day${i}`)/10}px`
+        }
+    }
+}
+
+function makeColorIfDayIsCompleted() {
+    
+}
 
 
 function setValuesForChartInMl () {
-    let value1 = document.querySelector('.dataValue1')
-    let value2 = document.querySelector('.dataValue2')
-    let value3 = document.querySelector('.dataValue3')
-    let value4 = document.querySelector('.dataValue4')
-    let value5 = document.querySelector('.dataValue5')
-    let value6 = document.querySelector('.dataValue6')
     if(localStorage.getItem('weight') == null){
-        value1.innerText = `${2400/6}`
-        value2.innerText = `${2400/6*2}`
-        value3.innerText = `${2400/6*3}`
-        value4.innerText = `${2400/6*4}`
-        value5.innerText = `${2400/6*5}`
-        value6.innerText = `${2400}`
+        calendar.values.value1.innerText = `${2400/6}`
+        calendar.values.value2.innerText = `${2400/6*2}`
+        calendar.values.value3.innerText = `${2400/6*3}`
+        calendar.values.value4.innerText = `${2400/6*4}`
+        calendar.values.value5.innerText = `${2400/6*5}`
+        calendar.values.value6.innerText = `${2400}`
     } else {
-        value1.innerText = `${Math.floor(localStorage.getItem('weight')*35/6)}`
-        value2.innerText = `${Math.floor(localStorage.getItem('weight')*35/6*2)}`
-        value3.innerText = `${Math.floor(localStorage.getItem('weight')*35/6*3)}`
-        value4.innerText = `${Math.floor(localStorage.getItem('weight')*35/6*4)}`
-        value5.innerText = `${Math.floor(localStorage.getItem('weight')*35/6*5)}`
-        value6.innerText = `${Math.floor(localStorage.getItem('weight')*35)}`
+        calendar.values.value1.innerText = `${Math.floor(localStorage.getItem('weight')*35/6)}`
+        calendar.values.value2.innerText = `${Math.floor(localStorage.getItem('weight')*35/6*2)}`
+        calendar.values.value3.innerText = `${Math.floor(localStorage.getItem('weight')*35/6*3)}`
+        calendar.values.value4.innerText = `${Math.floor(localStorage.getItem('weight')*35/6*4)}`
+        calendar.values.value5.innerText = `${Math.floor(localStorage.getItem('weight')*35/6*5)}`
+        calendar.values.value6.innerText = `${Math.floor(localStorage.getItem('weight')*35)}`
     
     }
     
@@ -53,6 +71,8 @@ function setNameForGreeting() {
 
 setValuesForChartInMl()
 setNameForGreeting()
+findAndSetValuesForToday()
+makeColorIfDayIsCompleted()
 
 
 //https://www.codester.com/items/18487/water-drinking-reminder-android-app-template
